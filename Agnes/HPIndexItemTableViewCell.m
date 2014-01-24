12c441dd11ee9f4c322da4687e8ac05db2fb4a25
hpique <hermes.pique@robotmedia.net>
2014-01-25 00:57:06 +0100

    Update icon and count when notes change

--- Agnes/HPIndexItemTableViewCell.m
+++ Agnes/HPIndexItemTableViewCell.m
@@ -8,6 +8,7 @@
 
 #import "HPIndexItemTableViewCell.h"
 #import "HPIndexItem.h"
+#import "HPNoteManager.h"
 
 @implementation HPIndexItemTableViewCell
 {
@@ -16,12 +17,49 @@
     __weak IBOutlet UIImageView *_iconView;
 }
 
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
+    if (self)
+    {
+        [self initHelper];
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self initHelper];
+}
+
+- (void)initHelper
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(objectsDidChange:) name:HPEntityManagerObjectsDidChangeNotification object:[HPNoteManager sharedManager]];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:HPEntityManagerObjectsDidChangeNotification object:[HPNoteManager sharedManager]];
+}
+
 - (void)setIndexItem:(HPIndexItem *)indexItem
 {
     _indexItem = indexItem;
     _titleView.text = self.indexItem.indexTitle;
     _iconView.image = self.indexItem.icon;
     _detailView.text = [NSString stringWithFormat:@"%ld", (long)self.indexItem.notes.count];
+}
+
+#pragma mark - Private
+
+- (void)objectsDidChange:(NSNotification*)notification
+{
+    if (self.indexItem)
+    {
+        _iconView.image = self.indexItem.icon;
+        _detailView.text = [NSString stringWithFormat:@"%ld", (long)self.indexItem.notes.count];        
+    }
 }
 
 @end
