9d373dcf1773e3aef85fc595b7fc2b6edf6c3803
hpique <hermespique@gmail.com>
2014-04-08 16:22:43 +0200

    Toggle index

--- Agnes/AGNRootPadViewController.m
+++ Agnes/AGNRootPadViewController.m
@@ -10,11 +10,20 @@
 #import "HPIndexItem.h"
 #import <Lyt/Lyt.h>
 
-@implementation AGNRootPadViewController
+static CGFloat const AGNIndexWidth = 240;
+
+@implementation AGNRootPadViewController {
+    NSLayoutConstraint *_indexLeftConstraint;
+    NSLayoutConstraint *_listLeftConstraint;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIImage *hashtagImage = [UIImage imageNamed:@"icon-hashtag"];
+    UIBarButtonItem *toggleIndexBarButtonItem = [[UIBarButtonItem alloc] initWithImage:hashtagImage style:UIBarButtonItemStylePlain target:self action:@selector(toggleIndexBarButtonAction:)];
+    self.listViewController.navigationItem.leftBarButtonItem = toggleIndexBarButtonItem;
     
     [self viewDidLoadAddChildViewController:self.indexNavigationController];
     [self viewDidLoadAddChildViewController:self.listNavigationController];
@@ -22,9 +31,9 @@
     {
         UIView *controllerView = self.indexNavigationController.view;
         controllerView.translatesAutoresizingMaskIntoConstraints = NO;
-        [controllerView lyt_setWidth:240];
+        [controllerView lyt_setWidth:AGNIndexWidth];
         [controllerView lyt_alignTopToParent];
-        [controllerView lyt_alignLeftToParent];
+        _indexLeftConstraint = [controllerView lyt_alignLeftToParent];
         [controllerView lyt_alignBottomToParent];
     }
     
@@ -32,10 +41,40 @@
         UIView *controllerView = self.listNavigationController.view;
         controllerView.translatesAutoresizingMaskIntoConstraints = NO;
         [controllerView lyt_alignTopToParent];
-        [controllerView lyt_placeRightOfView:self.indexNavigationController.view margin:1];
+        _listLeftConstraint = [controllerView lyt_placeRightOfView:self.indexNavigationController.view margin:1];
         [controllerView lyt_alignRightToParent];
         [controllerView lyt_alignBottomToParent];
     }
+}
+
+#pragma mark Actions
+
+- (void)toggleIndexBarButtonAction:(UIBarButtonItem*)barButtonItem
+{
+    const BOOL hide = _indexLeftConstraint.constant == 0;
+    if (hide)
+    {
+        [self.indexNavigationController viewWillDisappear:YES];
+    }
+    else
+    {
+        [self.indexNavigationController viewWillAppear:YES];
+    }
+    [self.indexNavigationController viewWillDisappear:YES];
+    [UIView animateWithDuration:0.3 animations:^{
+        _indexLeftConstraint.constant = hide ? - AGNIndexWidth : 0;
+        _listLeftConstraint.constant = hide ? 0 : 1;
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        if (hide)
+        {
+            [self.indexNavigationController viewDidDisappear:YES];
+        }
+        else
+        {
+            [self.indexNavigationController viewDidAppear:YES];
+        }
+    }];
 }
 
 @end
